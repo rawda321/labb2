0cd430585deee02167d69829a1ea76d7dc632791
User <https://github.com/rawda321/try1.git>
2022-03-15 15:04:10 +0300

    lab

---
+++ src/tr/edu/maltepe/oop/Person.java
@@ -0,0 +1,19 @@
+package tr.edu.maltepe.oop;
+
+public class Person {
+
+    private String name;
+    private int Id;
+
+
+    public int getId(){
+
+        return Id;
+    }
+    public String getName(){
+
+        return name;
+    }
+
+
+}
